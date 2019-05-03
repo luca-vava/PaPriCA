cbf045341aa33bcc0425dccd199dc340fedd4fbd
luca <33227886+luca-vava@users.noreply.github.com>
2019-05-03 16:37:20 +0200

    Aggiunta di commenti e pulizia codice

--- parallel_openMP.cpp
+++ parallel_openMP.cpp
@@ -11,25 +11,25 @@ int main()
 {	
  	using namespace std;
 
-	for(int k = 10; k<= 29; k++)
+	for(int k = 10; k<= 29; k++) 				//iterazione per test delarray sulle k diverse potenze di 2
 	{ 
- 		int threads=14;
+ 		int threads=14; 				//# di threads sui quali far girare il metodo di scan
  		int size = pow(2, k); 
 		double elapsed_secs = 0 ;
 
 
-		for (int i = 0; i<30; i++)
+		for (int i = 0; i<30; i++) 			//iterazione per calcolare un tempo medio su 30 cicli della procedura di scan
 		{
 
-			int *out = new int[size]; //allocazione array nell'heap
-			srand (time(NULL)); //randomizzazione del seed
+			int *out = new int[size];		//allocazione array nell'heap
+			srand (time(NULL)); 			//randomizzazione del seed
 
 			for (int j = 0; j < size; j++)
 			{
-				out[j] = rand()%100; // generazione numeri pseudocasuali 
+				out[j] = rand()%100; 		// generazione numeri pseudocasuali 
 			}
-
-			double start_time = omp_get_wtime();
+						 
+			double start_time = omp_get_wtime(); 	//calcolo e somma tempi esecuzione scan 	
 			scan(size, threads, out);
 			elapsed_secs += omp_get_wtime() - start_time;
 
@@ -53,14 +53,14 @@ void scan(int size, int threads, int out[])
 
 //////////////// downsweep phase
 
-    printf("-----------------DOWN----------------\n");
+    //printf("-----------------DOWN----------------\n");
 	
 	for (int k = 1; op_step >= 1; k++) 
 	{
 		offset = pow(2, k)-1;   
 		tot = pow(2, k-1);
 
-		#pragma omp parallel for num_threads(threads) ordered schedule(static) //firstprivate(op_step, offset, tot, out)
+	    #pragma omp parallel for num_threads(threads) schedule(static) 
 	    for (int i = offset; i <= size; i += offset+1)
 	    {
 	    	out[i] += out[i - tot];        
@@ -71,13 +71,13 @@ void scan(int size, int threads, int out[])
 	}
 
 	out[size-1] = 0;
-    printf("-----------------END - DOWN----------------\n");
+    //printf("-----------------END - DOWN----------------\n");
 
 ///////////////// upsweep phase
-    printf("-----------------UP----------------\n");
+    //printf("-----------------UP----------------\n");
 	for (int k=(size/2); k>0; k /=2)
 	{
-		#pragma omp parallel for schedule(static) num_threads(threads)
+		#pragma omp parallel for num_threads(threads) schedule(static) 
 		for (int i = 0; i<size; i+=k*2)
 		{
 			int tmp=out[i+k-1];
@@ -87,38 +87,6 @@ void scan(int size, int threads, int out[])
 		#pragma omp barrier
 	}
 
-	/*tot = size/2; 
-	op_step = 1;
-    while (op_step<=size/2) 
-	{
-		offset = size-1;   
-
-		#pragma omp parallel for schedule(static) num_threads(threads)  //firstprivate(offset, tot)//shared(op_step, offset, tot, out)
-	    for (int i = 1; i <= op_step; i++)
-	    {
-	    	int tmp = 0;
-	    	tmp = out[offset] + out[offset - tot];
-	    	out[offset-tot] = out[offset];
-	    	out[offset] = tmp;       
-	    	#pragma omp critical
-	    	{offset -= tot;
-	    	i*=2;}
-	    	//fprintf(prova, "op %d, offset %d, tot %d, tmp %d-------------------- \n", op_step, offset, tot, tmp);
-			//fprintf(prova, "------------------------------------------------- \n");
-	    }
-
-	   	tot /=2;
-		op_step *= 2;
-		fprintf(prova, "op %d, offset %d, tot %d, size %d-------------------- \n", op_step, offset, tot, size);
-
-		#pragma omp barrier
-	}*/
-	
-    /*for (int i = 0; i < size; ++i)
-	    {
-			fprintf(prova, "i: %d \n", out[i]);
-	 	}
-	*/
 	printf("-----------------END - UP----------------\n");
 }
 
